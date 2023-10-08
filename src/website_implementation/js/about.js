e2e504533032c77bcf48fdb94bf8f83665830422
CurtisYoung18 <Zhiyang.Mei@uqconnect.edu.au>
2023-10-08 15:35:40 +1000

    Weather api added
    I've added a weather element on page about using fetch-api and fixed some bugs.

---
+++ src/website_implementation/js/about.js
@@ -0,0 +1,37 @@
+// base URL of the Open Meteo API endpoint
+const baseUrl = 'https://api.open-meteo.com/v1/forecast';
+
+// query parameters as a JavaScript object
+const queryParams = {
+    latitude: -27.472984,
+    longitude: 153.079169,
+    current_weather: true,
+};
+
+// convert the query parameters
+const queryString = new URLSearchParams(queryParams).toString();
+
+// full URL with query parameters
+// give a name to the url
+const urlWithParams = baseUrl+"?"+queryString;
+
+// request options
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+};
+
+// make the fetch call
+fetch(urlWithParams, requestOptions)
+  // take the information from api and convert it into a JS Object
+  .then(response => response.json())
+  .then(data => {
+        // store the weather data into a const variable
+        const weather = data.current_weather
+        // there is a variable 'temperature' in the url or in the data of 'weather'
+        console.log("Current temperature: " + weather.temperature + "°C");
+        document.getElementById('weather-info').textContent = "Current temperature: " + weather.temperature + "°C";
+  })
+  .catch(error => console.log('error', error));
+
+
--- src/website_implementation/js/gallery.js
+++ src/website_implementation/js/gallery.js
@@ -2,7 +2,9 @@
 
 // Image Zoom Functionality for gallery.html
 document.addEventListener('DOMContentLoaded', function() {
+    // choose all the images
     let galleryItems = document.querySelectorAll('.gallery-item img');
+    // create new div and img elements for modal box and image.
     let modal = document.createElement('div');
     let modalImage = document.createElement('img');
     
@@ -11,6 +13,10 @@ document.addEventListener('DOMContentLoaded', function() {
     modal.appendChild(modalImage);
     document.body.appendChild(modal);
     
+
+    // For each image in the gallery, an event listener is added to listen for click events. 
+    // When an image is clicked, the src attribute of the image in the modal is set to the src of the clicked image, 
+    // and the modal is then displayed.
     galleryItems.forEach(item => {
         item.addEventListener('click', function() {
             modalImage.src = item.src;
@@ -18,21 +24,8 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
     
+    // click anywhere on the modal box will close it.
     modal.addEventListener('click', function() {
         modal.style.display = 'none';
     });
 });
-
-
-// Working with JS Objects
-
-let imageDetails = {
-    title: "Random Image",
-    description: "A random image fetched from an external API.",
-    source: "https://picsum.photos/",
-    displayImageDetails: function() {
-        console.log(`Title: ${this.title}, Description: ${this.description}, Source: ${this.source}`);
-    }
-};
-
-imageDetails.displayImageDetails();
