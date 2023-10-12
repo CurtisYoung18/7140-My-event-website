f5ad77fe9e35a6cc75c0a8062032f233c78d912c
CurtisYoung18 <Zhiyang.Mei@uqconnect.edu.au>
2023-10-12 17:50:24 +1000

    POSTing form to api
    post form message to external database api.

--- src/website_implementation/js/about.js
+++ src/website_implementation/js/about.js
@@ -16,4 +16,52 @@ const handleInputChange = () => {
     }
 };
 
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let firstName = document.getElementById('firstName').value;
+    let suburb = document.getElementById('suburb').value;
+    let email = document.getElementById('email').value;
+
+    let responseMessage = document.getElementById('responseMessage');
+
+    responseMessage.textContent = 'Sending your request ... please wait.';
+
+    let payload = {
+        subscriber_name: firstName,
+        subscriber_suburb: suburb,
+        subscriber_email: email
+    };
+
+    const url = 'https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/api/'
+    const method = 'POST';
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    fetch(url, {
+        method: method,
+        headers: headers,
+        body: JSON.stringify(payload)
+    })
+    .then((response) => response.text())
+    .then((data) => {
+        if (data === 'added') {
+            responseMessage.textContent ='Subscription successful. Thank you for subscribing!';
+        } else if (data === 'exists') {
+            responseMessage.textContent = 'This email address has already been used to subscribe.';
+        } else if (data == 'error') {
+                responseMessage.textContent = 'An error occured with the API. Please try again later.';
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        responseMessage.textContent = 'An unexpected error occured. Please try again later.';
+    });
+
+
+};
+
+
 subscribeForm.addEventListener('input',handleInputChange);
+subscribeForm.addEventListener('submit', handleSubmit);
