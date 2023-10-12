fbff0d0f4458f94d32eead97d9ada6d20525f902
CurtisYoung18 <Zhiyang.Mei@uqconnect.edu.au>
2023-10-12 17:02:52 +1000

    Form js
    I've added elements in about.js

--- src/website_implementation/js/about.js
+++ src/website_implementation/js/about.js
@@ -1,38 +1,19 @@
-// base URL of the Open Meteo API endpoint
-const baseUrl = 'https://api.open-meteo.com/v1/forecast';
+// get the subscribe form
+const subscribeForm = document.getElementById('subscribe-form');
 
-// query parameters as a JavaScript object
-const queryParams = {
-    latitude: -27.472984,
-    longitude: 153.079169,
-    current_weather: true,
+const handleInputChange = () => {
+    let firstName = document.getElementById('firstName');
+    let suburb = document.getElementById('suburb');
+    let email = document.getElementById('email');
+    let button = document.getElementById('subscribe-submit-button');
+
+    if (firstName.value && suburb.value && email.value && email.validity.valid){
+        button.classList.add('enabled');
+        button.disabled = false;
+    } else {
+        button.classList.remove('enabled');
+        button.disabled = true;
+    }
 };
 
-// convert the query parameters
-const queryString = new URLSearchParams(queryParams).toString();
-
-// full URL with query parameters
-// give a name to the url
-const urlWithParams = baseUrl+"?"+queryString;
-
-// request options
-const requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-};
-
-// make the fetch call
-fetch(urlWithParams, requestOptions)
-  // take the information from api and convert it into a JS Object
-  .then(response => response.json())
-  .then(data => {
-        // store the weather data into a const variable
-        const weather = data.current_weather
-        // there is a variable 'temperature' in the url or in the data of 'weather'
-        console.log("Current temperature: " + weather.temperature + "°C");
-        const temperature_element = document.getElementById('current_temperature');
-        const windspeed_element = document.getElementById('current_windspeed');
-        temperature_element.innerText = weather.temperature + "°C";
-        windspeed_element.innerText = weather.windspeed + "kph";
-  })
-  .catch(error => console.log('error', error));
+subscribeForm.addEventListener('input',handleInputChange);
