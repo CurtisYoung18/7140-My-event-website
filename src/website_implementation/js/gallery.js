80b76a199d33191de4c25c3de0165550e2830761
CurtisYoung18 <Zhiyang.Mei@uqconnect.edu.au>
2023-10-22 20:52:27 +1000

    comments for html and css

--- src/website_implementation/js/gallery.js
+++ src/website_implementation/js/gallery.js
@@ -7,11 +7,9 @@ The prompts used and the response from ChatGPT are included
 in Appendix 1.
 
 The output from these prompts was JavaScript codes
-which has the function of zooming images, fetching dynamic contents.
-I only apply the codes related to the zooming function.  
+which has the function of zooming images.
 
 This was used as below.
-
 */
 
 /*
@@ -19,41 +17,50 @@ DOM Manipulation and Usability Enhancements
 Image Zoom Functionality for gallery.html
 */
 document.addEventListener('DOMContentLoaded', function() {
-    let galleryItems = document.querySelectorAll('.gallery-item img');
-    let modal = document.createElement('div');
-    let modalContent = document.createElement('div');  // Create the modal content div
-    let modalImage = document.createElement('img');
+    let galleryItems = document.querySelectorAll('.gallery-item img'); // Select all the images inside the gallery items
+    let modal = document.createElement('div'); // Create a modal div to overlay the screen when an image is clicked
+    let modalContent = document.createElement('div'); // Create a div to hold the content of the modal
+    let modalImage = document.createElement('img'); // Create an image element for the modal to display the clicked image
     
+    // Add classes to the created elements for styling and selection
     modal.classList.add('image-modal');
-    modalContent.classList.add('modal-content');  // Add the class to the modal content div
+    modalContent.classList.add('modal-content'); 
     modalImage.classList.add('modal-img');
 
-    modalContent.appendChild(modalImage);  // Append the image to the modal content
+    // Append the image element to the modal content div
+    modalContent.appendChild(modalImage); 
+    
+    // Append the modal and its content to the body of the document
     document.body.appendChild(modal);
-    document.body.appendChild(modalContent);  // Append the modal content to the body
+    document.body.appendChild(modalContent); 
 
+    // Loop through each gallery item image
     galleryItems.forEach(item => {
         item.addEventListener('click', function() {
+            // Set the source of the modal image to the source of the clicked image
             modalImage.src = item.src;
+
+            // Display the modal and its content
             modal.style.display = 'block';
-            modalContent.style.display = 'block';  // Show the modal content
+            modalContent.style.display = 'block';
         });
     
-    document.addEventListener('keydown', function(event) {
-        if (event.key === "Escape") {
-            closeModal();
-        }
-    });
-        
+        document.addEventListener('keydown', function(event) {
+            // If the 'Escape' key is pressed, close the modal
+            if (event.key === "Escape") {
+                closeModal();
+            }
+        });
     });
     
-    // Close the modal when clicking anywhere
+    // Define a function to close the modal
     const closeModal = () => {
+        // Hide the modal and its content
         modal.style.display = 'none';
-        modalContent.style.display = 'none';  // Hide the modal content
+        modalContent.style.display = 'none';
     };
 
-    modal.addEventListener('click', closeModal);
+    modal.addEventListener('click', closeModal); // Close when clicking on modal 
     modalContent.addEventListener('click', closeModal);  // Close when clicking on modal content
     modalImage.addEventListener('click', closeModal);    // Close when clicking on the image itself
 });
