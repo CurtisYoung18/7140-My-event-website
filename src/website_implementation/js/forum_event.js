9ef1cd479efa07981c99635b322e4083ad699250
CurtisYoung18 <Zhiyang.Mei@uqconnect.edu.au>
2023-10-22 13:57:30 +1000

    remove the delete function

--- src/website_implementation/js/forum_event.js
+++ src/website_implementation/js/forum_event.js
@@ -13,7 +13,7 @@ const eventsContainer = document.getElementById('events-container');
 const filterDropdown = document.getElementById('filterDropdown');
 
 const queryString = new URLSearchParams(queryParams).toString();
-const urlWithParams = baseURLCommunityEvents+"?"+queryString;
+const urlWithParams = baseURLCommunityEvents + "?" + queryString;
 
 /* global variables */
 let events_list;
@@ -47,27 +47,27 @@ const handleFormSubmit = event => {
     }
 
     fetch(baseURLCommunityEvents, requestOptions)
-    .then(response => response.json().then(data => {
-        if (!response.ok) {
-            console.log("Sever response:", data);
-            throw new Error("Network response was not ok");    
-        }
-        return data;
-    }))
-    .then(data => {
-        console.log(data.description);
-        photoFileInputLabel.textContent = "Add a photo (Optional)";
-        alert(`Your event "${data.description}" has been added to our website! Thanks!`);
-        eventForm.reset();
-        return data;
-    })
-    .then(data => {
-        getCommunityEvents();
-    })
-    .catch(error => {
-        console.error("There was a problem with the fetch operation:", error.message);
-        alert("Error submitting event. Please try again.");
-    });
+        .then(response => response.json().then(data => {
+            if (!response.ok) {
+                console.log("Sever response:", data);
+                throw new Error("Network response was not ok");
+            }
+            return data;
+        }))
+        .then(data => {
+            console.log(data.description);
+            photoFileInputLabel.textContent = "Add a photo (Optional)";
+            alert(`Your event "${data.description}" has been added to our website! Thanks!`);
+            eventForm.reset();
+            return data;
+        })
+        .then(data => {
+            getCommunityEvents();
+        })
+        .catch(error => {
+            console.error("There was a problem with the fetch operation:", error.message);
+            alert("Error submitting event. Please try again.");
+        });
 };
 
 // render events after fetch or filter
@@ -77,22 +77,20 @@ const renderEvents = (eventsToRender) => {
     }
     eventsToRender.forEach(event => {
         const eventTemplate = `
-                <article class="col-12 col-md-12 col-lg-6" id="card${event.id}">
-                    <div class="card" role="group" aria-labelledby="card${event.id}-title" aria-describedby="card${event.id}-desc">
-                        <h2 class="card-header p-2" id="card${event.id}-title">${event.name}</h2>
-                        <img class="card-banner-image" src="${event.photo}" alt="${event.name}">
-                        <p class="card-body-text p-2">${event.description}</p>
-                        <p class="card-body-text px-2"><strong>Location:</strong>${event.location}</p>
-                        <p class="card-body-text px-2"><strong>Organiser:</strong>${event.organiser}</p>
-                        <p class="card-body-text px-2"><strong>Event Type:</strong>${event.event_type}</p>
-                        <p class="card-body-text px-2"><strong>Date & Time:</strong>${new Date(event.date_time).toLocaleString()}</p>
-                        <button data-id="${event.id}" onclick="deleteCardById(this)">Delete</button>
-                    </div>
-                </article>
-            `;
-            eventsContainer.innerHTML += eventTemplate;
+            <article class="col-12 col-md-12 col-lg-6" id="card${event.id}">
+                <div class="card" role="group" aria-labelledby="card${event.id}-title" aria-describedby="card${event.id}-desc">
+                    <h2 class="card-header p-2" id="card${event.id}-title">${event.name}</h2>
+                    <img class="card-banner-image" src="${event.photo}" alt="${event.name}">
+                    <p class="card-body-text p-2">${event.description}</p>
+                    <p class="card-body-text px-2"><strong>Location:</strong>${event.location}</p>
+                    <p class="card-body-text px-2"><strong>Organiser:</strong>${event.organiser}</p>
+                    <p class="card-body-text px-2"><strong>Event Type:</strong>${event.event_type}</p>
+                    <p class="card-body-text px-2"><strong>Date & Time:</strong>${new Date(event.date_time).toLocaleString()}</p>
+                </div>
+            </article>
+        `;
+        eventsContainer.innerHTML += eventTemplate;
     })
-
 }
 
 // fetching events from Community Events API
@@ -101,76 +99,40 @@ const getCommunityEvents = () => {
         website_code: my_website_code,
     }
     const queryString = new URLSearchParams(queryParams).toString();
-    const urlWithParams = baseURLCommunityEvents+"?"+queryString;
+    const urlWithParams = baseURLCommunityEvents + "?" + queryString;
     const requestOptions = {
         method: 'GET',
         redirect: 'follow'
     }
 
-    
     fetch(urlWithParams, requestOptions)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        return response.json();
-    })
-    .then(events => {
-        events_list = events;
-        renderEvents(events_list);
-    })
-    .catch(error => {
-        console.error("Error processing events:", error.message);
-        alert("There was a problem loading events. Please refresh the page to try again.");
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then(events => {
+            events_list = events;
+            renderEvents(events_list);
+        })
+        .catch(error => {
+            console.error("Error processing events:", error.message);
+            alert("There was a problem loading events. Please refresh the page to try again.");
+        });
 };
 
 // event filtering
 const filterEventByTerm = (term) => {
-
     // if no term selected, show all events
     if (!term) {
         renderEvents(events_list);
         return;
     }
-
     // filter events by the term and then render them 
     const filteredEvents = events_list.filter(event => event.event_type.includes(term));
     renderEvents(filteredEvents);
 }
-// chat-gpt define the delete function
-function deleteCardById(buttonElement) {
-    const eventId = buttonElement.getAttribute('data-id');
-    const deleteUrl = `${baseURLCommunityEvents}${eventId}`;
-
-    console.log("Trying to delete event with ID:", eventId); // Log the event ID
-    console.log("DELETE request URL:", deleteUrl); // Log the request URL
-
-    // Send a DELETE request to the API to delete the event
-    fetch(deleteUrl, {
-        method: 'DELETE',
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        return response.json();
-    })
-    .then(() => {
-        // If the deletion was successful, remove the card from the UI
-        const cardElement = document.querySelector(`div[aria-labelledby="card${eventId}-title"]`).parentElement;
-        if (cardElement) {
-            cardElement.remove();
-            alert("Event successfully deleted!");
-        }
-    })
-    .catch(error => {
-        console.error("There was a problem with the delete operation:", error.message);
-        alert("Error deleting event. Please try again.");
-    });
-}
-
-
 
 /* event listeners */
 photoFileInputLabel.addEventListener('click', triggerFileInput);
